f894b9f3cc786897b69be4fa8c961b109b525470
kglanos <130902847+kglanos@users.noreply.github.com>
2023-10-01 01:48:29 +0200

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,3 +1,30 @@
 import axios from "axios";
+export { fetchBreeds, fetchCatByBreed };
 
-axios.defaults.headers.common["x-api-key"] = "live_vOiPLNODznPHBOW7PVMtKMDgacOGjcgagL1gHoS4OMosoNzDXQtbMukwVv049dOU";
+axios.defaults.headers.common['x-api-key'] = 'live_vOiPLNODznPHBOW7PVMtKMDgacOGjcgagL1gHoS4OMosoNzDXQtbMukwVv049dOU';
+
+function fetchBreeds() {
+    const apiUrl = `https://api.thecatapi.com/v1/breeds`;
+
+    return axios.get(apiUrl)
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            console.error('An error occurred while downloading races:', error);
+            return Promise.reject(error);
+        });
+}
+
+function fetchCatByBreed(breedId) {
+    const apiUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+
+    return axios.get(apiUrl)
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            console.error('An error occurred while downloading races:', error);
+            return Promise.reject(error);
+        });
+}
---
+++ src/index.js
@@ -0,0 +1,111 @@
+import axios from 'axios';
+import { fetchBreeds } from './cat-api.js';
+import { fetchCatByBreed } from './cat-api.js';
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
+const reference = {
+
+    breedSelect: document.querySelector('.breed-select'),
+    catInfo: document.querySelector('.cat-info'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error')
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    reference.error.classList.add('hidden');
+    reference.loader.classList.add('visible');
+});
+
+
+
+fetchBreeds() 
+.then(breeds => {
+
+    const firstElement = `<option>Please, select a cat</option>`;
+    reference.breedSelect.insertAdjacentHTML('afterbegin', firstElement);
+
+    const markupSelect = breeds
+        .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+        .join(' ');
+
+        reference.breedSelect.insertAdjacentHTML('afterbegin', markupSelect);
+
+        const slimSelect = new SlimSelect({
+            select: reference.breedSelect,
+            placeholder: 'Please, select a cat',
+            showSearch: true,
+        });
+
+        reference.loader.classList.remove('visible');
+        reference.loader.classList.add('hidden');
+})
+
+.catch(error => {
+    console.error(error);
+    reference.loader.classList.remove('visible');
+    reference.loader.classList.add('hidden');
+    reference.error.classList.remove('hidden');
+    reference.error.classList.add('visible');
+    Notiflix.Notify.failure('An error occurred while fetching breeds.');
+});
+
+reference.breedSelect.addEventListener('change', onchange);
+
+function onchange(event) {
+    const breedId = reference.breedSelect.value;
+
+    reference.loader.classList.remove('hidden');
+    reference.loader.classList.add('visible');
+
+    fetchCatByBreed(breedId)
+        .then(catInfo => {
+            if(catInfo.length === 0) {
+                reference.loader.classList.remove('visible');
+                reference.loader.classList.add('hidden');
+                reference.catInfo.classList.add('hidden');
+                reference.error.textContent = "No cats found.";
+                reference.error.classList.remove('hidden');
+                Notiflix.Notify.warning('No cats found.');
+            } else {
+                reference.error.classList.add('hidden');
+                reference.catInfo.classList.remove('hidden');
+        
+                const liArray = catInfo.map(cat => {
+                    const { url, breeds } = cat;
+                    const breedItems = breeds.map(breed => {
+                    return `
+                        <div class="image-container">
+                        <img class="cat-image" src="${url}" alt="${breed.name}">
+                        <div class="text-container"> 
+                            <h1 class="title">${breed.name}</h1>
+                            <p class="main-text">${breed.description}</p>
+                            <p class="secondary-title">Temperament: ${breed.temperament}</p>
+                        </div>
+                        </div>
+                    `;
+                });
+                    return breedItems.join(' ');
+                })
+
+                const markup = liArray.join(' ');
+                reference.catInfo.innerHTML = markup;
+
+                reference.loader.classList.remove('visible');
+                reference.loader.classList.add('hidden');
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            reference.loader.classList.remove("visible");
+            reference.loader.classList.add('hidden');
+            reference.error.classList.remove('hidden');
+            reference.error.classList.add('visible');
+            Notiflix.Notify.failure('An error occurred while fetching cat information.');
+        })
+        .finally(() => {
+            reference.loader.classList.remove("visible")
+        })
+}
+
+
