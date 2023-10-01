20e1074e7042507a769829b04a3f04d4855d174c
kglanos <130902847+kglanos@users.noreply.github.com>
2023-10-01 02:24:07 +0200

    update 2

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const reference = {
     error: document.querySelector('.error')
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
-    reference.error.classList.add('hidden');
-    reference.loader.classList.add('visible');
+    reference.loader.classList.add("visible");
 });
 
 
@@ -37,16 +37,14 @@ fetchBreeds()
             showSearch: true,
         });
 
-        reference.loader.classList.remove('visible');
-        reference.loader.classList.add('hidden');
+        reference.loader.classList.remove("visible");
+        reference.loader.classList.add("hidden");
 })
 
 .catch(error => {
     console.error(error);
-    reference.loader.classList.remove('visible');
-    reference.loader.classList.add('hidden');
-    reference.error.classList.remove('hidden');
-    reference.error.classList.add('visible');
+    reference.loader.classList.remove("visible");
+    reference.loader.classList.add("hidden");
     Notiflix.Notify.failure('An error occurred while fetching breeds.');
 });
 
@@ -55,32 +53,30 @@ reference.breedSelect.addEventListener('change', onchange);
 function onchange(event) {
     const breedId = reference.breedSelect.value;
 
-    reference.loader.classList.remove('hidden');
-    reference.loader.classList.add('visible');
+    reference.loader.classList.remove("hidden");
+    reference.loader.classList.add("visible");
 
     fetchCatByBreed(breedId)
         .then(catInfo => {
             if(catInfo.length === 0) {
-                reference.loader.classList.remove('visible');
-                reference.loader.classList.add('hidden');
-                reference.catInfo.classList.add('hidden');
-                reference.error.textContent = "No cats found.";
-                reference.error.classList.remove('hidden');
+                reference.loader.classList.remove("visible");
+                reference.loader.classList.add("hidden");
+                reference.catInfo.classList.add("hidden");
                 Notiflix.Notify.warning('No cats found.');
             } else {
-                reference.error.classList.add('hidden');
-                reference.catInfo.classList.remove('hidden');
+                reference.error.classList.add("hidden");
+                reference.catInfo.classList.remove("hidden");
         
                 const liArray = catInfo.map(cat => {
                     const { url, breeds } = cat;
                     const breedItems = breeds.map(breed => {
                     return `
                         <div class="image-container">
-                        <img class="cat-image" src="${url}" alt="${breed.name}">
+                        <img class="image-cat" src="${url}" alt="${breed.name}">
                         <div class="text-container"> 
                             <h1 class="title">${breed.name}</h1>
-                            <p class="main-text">${breed.description}</p>
-                            <p class="secondary-title">Temperament: ${breed.temperament}</p>
+                            <p class="first-text">${breed.description}</p>
+                            <p class="second-text">Temperament: ${breed.temperament}</p>
                         </div>
                         </div>
                     `;
@@ -91,16 +87,14 @@ function onchange(event) {
                 const markup = liArray.join(' ');
                 reference.catInfo.innerHTML = markup;
 
-                reference.loader.classList.remove('visible');
-                reference.loader.classList.add('hidden');
+                reference.loader.classList.remove("visible");
+                reference.loader.classList.add("hidden");
             }
         })
         .catch(error => {
             console.error(error);
             reference.loader.classList.remove("visible");
-            reference.loader.classList.add('hidden');
-            reference.error.classList.remove('hidden');
-            reference.error.classList.add('visible');
+            reference.loader.classList.add("hidden");
             Notiflix.Notify.failure('An error occurred while fetching cat information.');
         })
         .finally(() => {
